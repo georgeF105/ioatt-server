beaba5496e8c701c255af4ee4973d55e05bda78e
george francis <georgefrancis105@gmail.com>
2017-03-13 20:13:59 +1300

    Update existing tables

--- migrations/20170304152506_devices.js
+++ migrations/20170304152506_devices.js
@@ -3,7 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('devices', (table) => {
     table.increments('id').primary();
     table.string('name').unique().notNullable();
-    table.string('description').notNullable();
+    table.string('description');
+    table.interger('location_id').references('id').inTable('location');
   });
 };
 
--- migrations/20170304184221_users.js
+++ migrations/20170304184221_users.js
@@ -2,7 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('users', (table) => {
     table.increments('id').primary();
-    table.string('name').unique().notNullable();
+    table.string('name').notNullable();
+    table.string('email').unique();
   });
 };
 
