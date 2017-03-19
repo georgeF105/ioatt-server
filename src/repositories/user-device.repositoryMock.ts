71877198bb1da47376e488e348adcc13458c2214
george francis <georgefrancis105@gmail.com>
2017-03-19 14:00:46 +1300

    Make user-device.service tests

---
+++ src/repositories/user-device.repositoryMock.ts
@@ -0,0 +1,14 @@
+import { Observable } from 'rxjs/Observable';
+import { IUserDeviceRepository } from './user-device.repository';
+
+import { IDevice } from '../interfaces/IDevice';
+
+export default class UserDeviceRepositoryMock implements IUserDeviceRepository {
+  public getUserJoins (userId: number): Observable<any[]> {
+    throw('mock not stubed - getUserJoins');
+  }
+
+  public getDeviceJoins(deviceId: number): Observable<any[]> {
+    throw('mock not stubbed - getDeviceJoins');
+  }
+}
